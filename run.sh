47eedc7c81cbbc41fe9d4d1832f8591a8bcb5c9a
pappik <paparao.prema75@gmail.com>
2025-03-15 16:19:17 -0500

    expense backend and a shell command added

---
+++ run.sh
@@ -0,0 +1,16 @@
+env=$1
+action=$2
+
+if [ -z "$env" ]; then
+  echo "env is not provided(dev|qa|stage|prod)"
+  exit
+fi
+
+if [ -z "$action" ]; then
+  echo "action is not provided(apply | destroy)"
+  exit
+fi
+
+rm -rf .terraform/terraform.tfstate
+terraform init -backend-config= env-$env/state.tfvars
+terraform $action -var-file=env-$env/main.tfvars
