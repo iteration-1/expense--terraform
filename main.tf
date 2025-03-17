8b685685fc40fad44f5426d0d7efd2643080f363
pappik <paparao.prema75@gmail.com>
2025-03-17 14:48:08 -0500

    added mysql and backend modules

--- main.tf
+++ main.tf
@@ -10,3 +10,27 @@ module "frontend" {
 
 
 }
+module "backend" {
+  source    = "./modules/app"
+  component = "backend"
+
+  env            = var.env
+  instance_type = var.instance_type
+  ssh_user      = var.ssh_user
+  ssh_password  = var.ssh_password
+  zone_id       = var.zone_id
+
+
+}
+module "mysql" {
+  source    = "./modules/app"
+  component = "mysql"
+
+  env            = var.env
+  instance_type = var.instance_type
+  ssh_user      = var.ssh_user
+  ssh_password  = var.ssh_password
+  zone_id       = var.zone_id
+
+
+}
